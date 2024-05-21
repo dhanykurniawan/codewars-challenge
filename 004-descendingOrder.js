687d4249819d685d5b22ce3b3e6619e071f67356
Dhany Kurniawan <dhanykurniawan111@gmail.com>
2024-05-21 09:25:48 +0700

    first commit

---
+++ 004-descendingOrder.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+function descendingOrder(n) {
+  return parseInt(
+    n
+      .toString()
+      .split("")
+      .sort((a, b) => b - a)
+      .join("")
+  );
+}
